eab0fbd83086a6f7d063d603f24e41e7a247f276
simaipeter <simaipeter@gmail.com>
2016-10-30 09:20:33 +0100

    Update spookydayz1.js

--- spookydayz1.js
+++ spookydayz1.js
@@ -14,7 +14,7 @@ function init() {
 	spoky = document.createElement("div");
 	spoky.id = "spookyi";
 	$("body").append(spoky);
-	$("#spookyi").css({"width":"26%", "height":"3%", "background":"#d2a990","color":"white", "position":" absolute", "top":"22px", "left":"2px", "z-index":"1", "opacity":"0.6", "overflow-x":"hidden", "overflow-y":"scroll "}) ;
+	$("#spookyi").css({"width":"26%", "height":"3%", "background":"#d2a990","color":"white", "position":" relative", "bottom":"-22px", "left":"2px", "z-index":"1", "opacity":"0.6", "overflow-x":"hidden", "overflow-y":"scroll "}) ;
 	$("#spookylogger").css({"width":"26%", "height":"17%", "background":"#e79153","color":"white", "position":" absolute", "top":"22px", "left":"2px", "z-index":"1", "opacity":"0.6", "overflow-x":"hidden", "overflow-y":"scroll "}) ;
 
 setTimeout("spookylogger()",1000)
