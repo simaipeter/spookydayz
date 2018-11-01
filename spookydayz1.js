54568a17bd25d9438fb1c0b4a16a5fbd0883cf4b
spepe <simai.peter@gmail.com>
2018-11-01 13:14:44 +0100

    0.3

--- spookydayz1.js
+++ spookydayz1.js
@@ -1,4 +1,9 @@
+i = document.createElement("link");
+i.rel = 'stylesheet';
+i.href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" ;
+document.head.appendChild(i) ;
 var jq = document.createElement('script');
+
 jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
 // ... give time for script to load, then type (or see below for non wait option)
@@ -6,6 +11,7 @@ jQuery.noConflict();
 ju = document.createElement('script' );
 ju.src = "https://code.jquery.com/ui/1.12.1/jquery-ui.js";
 document.head.appendChild(ju);
+
 
 setTimeout("init()",2000);
 
@@ -15,11 +21,11 @@ function init() {
 	spoky = document.createElement("div");
 	spoky.id = "spookylogger";
 	$("body").append(spoky); 
-	spoky = document.createElement("div");
-	spoky.id = "spookyi";
-	$("body").append(spoky);
-	$("#spookyi").css({"width":"343px", "height":"44px","font-size":"9pt", "background":"#d2a990","color":"white","position":"absolute","top":"185px" }) ;
-	$("#spookylogger").css({"width":"341px", "height":"162.1px", "background":"#e79153","color":"white", "position":" absolute", "top":"22px", "left":"2px", "z-index":"1", "opacity":"0.6", "overflow-x":"hidden", "overflow-y":"scroll "}) ;
+	spoky1 = document.createElement("div");
+	spoky1.id = "spookyi";
+	$("#spookylogger").append(spoky1);
+	//$("#spookyi").css({"width":"343px", "height":"44px","font-size":"9pt", "background":"#d2a990","color":"white","position":"absolute","top":"185px" }) ;
+	//$("#spookylogger").css({"width":"341px", "height":"162.1px", "background":"#e79153","color":"white", "position":" absolute", "top":"22px", "left":"2px", "z-index":"1", "opacity":"0.6", "overflow-x":"hidden", "overflow-y":"scroll "}) ;
 
 setTimeout("spookylogger()",1000)
 setInterval("spookylogger()",122);
@@ -100,3 +106,4 @@ function log() {
 	$('#spookylogger').append($('#spo0kyD').html()) ;
 	
 }
+
